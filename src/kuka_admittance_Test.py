5084366ab85b9e1d89731332ff14352fffee2fb6
Heejin09 <whj03319@gmail.com>
2023-01-19 16:16:23 +0900

    change source

--- src/kuka_admittance_Test.py
+++ src/kuka_admittance_Test.py
@@ -15,14 +15,23 @@ planeID = p.loadURDF("plane.urdf")
 cubeStartPos = [0, 0, 0]
 cubeStartOrientation = p.getQuaternionFromEuler([0, 0, 3.14])
 robotID = p.loadURDF("C:/Users/whj03/Desktop/admittance/urdf/kr210l150_link.urdf", cubeStartPos, cubeStartOrientation)
-p.setGravity(0, 0, 0)
+p.setGravity(0, 0, -9.8)
 p.setTimeStep(1/200)
 time_step = 1/200
 
-for i in range(0,p.getNumJoints(robotID)):
-	print(i,p.getJointInfo(robotID,i))
+for i in range(0, p.getNumJoints(robotID)):
+	print(i, p.getJointInfo(robotID, i))
 
+def getJointPosition(ID,join_list):
+	JointStates = p.getJointStates(ID, join_list);
+	JointPositions = [JointStates[0][0], JointStates[1][0], JointStates[2][0], JointStates[3][0], JointStates[4][0], JointStates[5][0]]
+	return JointPositions
+
+def getJointVelocities(ID,join_list):
+	JointStates = p.getJointStates(ID, join_list);
+	JointVelocities = [JointStates[0][1], JointStates[1][1], JointStates[2][1], JointStates[3][1], JointStates[4][1], JointStates[5][1]]
+	return JointVelocities
 if __name__ == "__main__":
-
+	for i in range(10000):
 		p.stepSimulation();
 		time.sleep(time_step)
